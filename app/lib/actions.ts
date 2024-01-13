8ad8fe496f075d01d6f9e2cb96ed8014c70d0020
zeusopera9 <zaidalimerchant@yahoo.com>
2024-01-13 20:31:36 +0530

    Chapter 15 Done

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,7 @@
 'use server';
 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
@@ -110,4 +112,23 @@ export async function createInvoice(prevState: State, formData: FormData) {
       }
     }
     revalidatePath('/dashboard/invoices');
+  }
+
+  export async function authenticate (
+    prevState: string | undefined,
+    formData: FormData,
+  ) {
+    try {
+      await signIn('credentials', formData);
+    } catch(error) {
+      if(error instanceof AuthError) {
+        switch(error.type) {
+          case 'CredentialsSignin':
+            return 'Invalid Credentials.';
+          default:
+            return 'Something went wrong.';
+        }
+      }
+      throw error;
+    }
   }
---
+++ auth.config.ts
@@ -0,0 +1,23 @@
+import type { NextAuthConfig } from 'next-auth';
+
+export const authConfig = {
+    pages: {
+        signIn: '/login',
+    },
+    callbacks: {
+        authorized({ auth, request: { nextUrl } }) {
+            const isLoggedIn = !!auth?.user;
+            const isOnDasboard = nextUrl.pathname.startsWith('/dashboard');
+            if(isOnDasboard) {
+                if(isLoggedIn) {
+                    return true;
+                }
+                return false;
+            } else if(isLoggedIn) {
+                return Response.redirect(new URL('/dashboard', nextUrl));
+            }
+            return true;
+        },
+    },
+    providers: [],
+}satisfies NextAuthConfig;
