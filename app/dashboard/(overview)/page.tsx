e2fd7855199c9003531376518a9bf360afadfabe
zeusopera9 <zaidalimerchant@yahoo.com>
2024-01-14 14:36:14 +0530

    Chapter 16 Done

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -5,7 +5,12 @@ import { lusitana } from '@/app/ui/fonts';
 import { fetchCardData } from '../../lib/data';
 import { Suspense } from 'react';
 import { LatestInvoicesSkeleton, RevenueChartSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
- 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Overview',
+};
+
 export default async function Page() {
     
     const {
